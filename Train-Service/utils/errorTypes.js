19cc3e1689601b2f80d1fe26c9005bdc45c3d487
Anurag pandey <149105080+anuragyes@users.noreply.github.com>
2026-03-06 16:10:27 +0530

    Add files via upload

---
+++ Train-Service/utils/errorTypes.js
@@ -0,0 +1,56 @@
+class AppError extends Error {
+    constructor(message, statusCode, code) {
+        super(message);
+        this.statusCode = statusCode;
+        this.code = code;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+class BadRequestError extends AppError {
+    constructor(message, code = "BAD_REQUEST") {
+        super(message, 400, code);
+    }
+}
+
+class NotFoundError extends AppError {
+    constructor(message, code = "NOT_FOUND") {
+        super(message, 404, code);
+    }
+}
+
+class UnauthorizedError extends AppError {
+    constructor(message, code = "UNAUTHORIZED") {
+        super(message, 401, code);
+    }
+}
+
+class ForbiddenError extends AppError {
+    constructor(message, code = "FORBIDDEN") {
+        super(message, 403, code);
+    }
+}
+
+class ConflictError extends AppError {
+    constructor(message, code = "CONFLICT") {
+        super(message, 409, code);
+    }
+}
+
+const ToomanyRequestsError = class extends AppError {
+    constructor(message, code = "TOO_MANY_REQUESTS") {
+        super(message, 429, code);
+    }
+}
+
+
+module.exports = {
+    AppError,
+    ToomanyRequestsError,
+    BadRequestError,
+    NotFoundError,
+    UnauthorizedError,
+    ForbiddenError,
+    ConflictError
+};
